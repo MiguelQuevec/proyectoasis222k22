b9c4d46875608b1e98f2fa3c5051de1d8cc0c345
KevinAlejandroCol8 <kevin_gonzalez16@outlook.es>
2022-08-07 20:47:11 -0600

    Almacenamiento

---
+++ G4/SistemaReparto/src/main/java/trabajadores/controlador/AlmacenamientoPediso.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajadores.controlador;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class AlmacenamientoPediso {
+    private String idAlmacenamiento, idAsignacion, isBodega,estado;
+
+    public String getIdAlmacenamiento() {
+        return idAlmacenamiento;
+    }
+
+    public void setIdAlmacenamiento(String idAlmacenamiento) {
+        this.idAlmacenamiento = idAlmacenamiento;
+    }
+
+    public String getIdAsignacion() {
+        return idAsignacion;
+    }
+
+    public void setIdAsignacion(String idAsignacion) {
+        this.idAsignacion = idAsignacion;
+    }
+
+    public String getIsBodega() {
+        return isBodega;
+    }
+
+    public void setIsBodega(String isBodega) {
+        this.isBodega = isBodega;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
